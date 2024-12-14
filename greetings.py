8e09a63e0e2003b33a713ab047fe7a9d713fe79c
olegzos <olegzos991@gmail.com>
2024-12-14 23:35:25 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Oleg' #имя пользователя
+print('username:', username)
+title = 'Grade 1'  #заголовок заметки
+print('title:', title)
+content = 'Task 1' #описание заметки
+print('content:', content)
+status = 'Actual' #статус заметки
+print('status:', status)
+created_date = '11-12-2024' #дата создания заметки в формате
+print('created_date:', created_date)
+issue_date = '18-12-2024' #дата истечения заметки
+print('issue_date:', issue_date)
