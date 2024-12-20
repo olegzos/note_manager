286d3696590121cfb33d513fa8c5501b0f65cbd3
olegzos <olegzos991@gmail.com>
2024-12-20 15:00:34 +0300

    date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,12 +1,12 @@
-username = 'Oleg' #имя пользователя
+username = 'Oleg'  # имя пользователя
 print('username:', username)
-title = 'Grade 1'  #заголовок заметки
+title = 'Grade 1'  # заголовок заметки
 print('title:', title)
-content = 'Task 1' #описание заметки
+content = 'Task 1'  # описание заметки
 print('content:', content)
-status = 'Actual' #статус заметки
+status = 'Actual'  # статус заметки
 print('status:', status)
-created_date = '11.12.2024' #дата создания заметки
-print('created_date:',created_date [:6])
-issue_date = '18.12.2024' #дата истечения заметки
-print('issue_date:',issue_date [:6])
+created_date = '11.12.2024'  # дата создания заметки
+print('created_date:', created_date[:6])
+issue_date = '18.12.2024'  # дата истечения заметки
+print('issue_date:', issue_date[:6])
